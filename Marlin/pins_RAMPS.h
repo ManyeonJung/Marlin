990e040fbb297b1b6456d0dfe8a60d69d4af07a9
Manyeon Jung <jmy1330@gmail.com>
2018-05-30 00:45:18 +0900

    Modified : Delta_mini configuration

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -223,8 +223,8 @@
   #define HEATER_1_PIN   RAMPS_D9_PIN
   #define HEATER_BED_PIN RAMPS_D8_PIN
 #elif ENABLED(IS_RAMPS_EFF)                    // Hotend, Fan, Fan
-  #define FAN_PIN        RAMPS_D9_PIN
-  #define FAN1_PIN       RAMPS_D8_PIN
+  #define FAN_PIN        RAMPS_D8_PIN
+  #define FAN1_PIN       RAMPS_D9_PIN
 #elif ENABLED(IS_RAMPS_SF)                     // Spindle, Fan
   #define FAN_PIN        RAMPS_D8_PIN
 #else                                          // Non-specific are "EFB" (i.e., "EFBF" or "EFBE")
